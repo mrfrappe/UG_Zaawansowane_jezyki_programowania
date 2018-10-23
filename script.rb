61f9d8bc0bfc64cdb4e8d5849ac9d33e4b0eba37
Karol Kawski <karol.kawski@protonmail.com>
2018-10-24 01:12:19 +0200

    if else windows width

--- script.rb
+++ script.rb
@@ -28,13 +28,20 @@ end
 class Snail < Sprite
     def self.info()
     end
-    def setXCord(x, value)
-        x = Random.new().rand(x - value...x + value)
+    def setXCord(x, w, value)
+        if x <= w and x > 0
+            x = Random.new().rand(x - value...x + value)
+        else
+            x = x+10
+        end
 #        puts "X: #@x"
     end   
-    def setYCord(y, value)
-        y = Random.new().rand(y - value... y+ value)
-#       puts "Y: #@y"
+    def setYCord(y, h, value)
+        if y <= h and y > 0
+            y = Random.new().rand(y - value... y+ value)
+        else
+            y = y+10
+        end
     end
     def eat 
         #eat fun
@@ -52,24 +59,23 @@ tick = 0
 board = Board.new(500,500, "Boardgame")
 Board.info()
 set title: board.getName
-set background: 'green'
+set background: '#00B200'
 set resizable: false
 set diagonostics: true
 set width: board.getWidth
 set height: board.getHeight
 
 while $i <= $howManySnails do
-    ary.push(Snail.new('art.png',x: Random.new().rand(board.getWidth), y: Random.new().rand(board.getHeight), width: 10, height: 10, color: 'black', z: 20))
+    ary.push(Snail.new('art.png',x: Random.new().rand(board.getWidth), y: Random.new().rand(board.getHeight), width: 10, height: 10, z: 20))
     Snail.info()
     $i += 1
 end
-
 update do
   if tick % 10 == 0
       for j in 0..ary.length-1 
           ary[j].remove
-          ary[j].x = ary[j].setXCord(ary[j].x, 10)
-          ary[j].y = ary[j].setYCord(ary[j].y, 10)
+          ary[j].x = ary[j].setXCord(ary[j].x, board.getWidth, 10)
+          ary[j].y = ary[j].setYCord(ary[j].y, board.getHeight, 10)
           ary[j].add
           j += 1
       end
