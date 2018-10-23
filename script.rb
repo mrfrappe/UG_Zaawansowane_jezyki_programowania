d4b299898cdeeb19ce482ccfb69802c7434da277
Karol Kawski <karol.kawski@protonmail.com>
2018-10-24 00:34:39 +0200

    fix

--- script.rb
+++ script.rb
@@ -28,21 +28,13 @@ end
 class Snail < Rectangle
     def self.info()
     end
-    def getXCord
-        @x
-    end
-    def getYCord
-        @y
-    end
-    def setXCord(value)
-        xRand = Random.new()
-        @x = xRand.rand(@x- value...@x + value)
-        puts "X: #@x"
+    def setXCord(x, value)
+        x = Random.new().rand(x - value...x + value)
+#        puts "X: #@x"
     end   
-    def setYCord(value)
-        yRand = Random.new()
-        @y = yRand.rand(@y- value... @y+ value)
-       puts "Y: #@y"
+    def setYCord(y, value)
+        y = Random.new().rand(y - value... y+ value)
+#       puts "Y: #@y"
     end
     def eat 
         #eat fun
@@ -54,7 +46,7 @@ end
 
 ary = Array.new 
 $i = 1
-$howManySnails = 10
+$howManySnails = 100
 tick = 0
 
 board = Board.new(500,500, "Boardgame")
@@ -67,10 +59,7 @@ set width: board.getWidth
 set height: board.getHeight
 
 while $i <= $howManySnails do
-    xRand = Random.new()
-    yRand = Random.new()
-    snail=Snail.new(x: xRand.rand(board.getWidth), y: yRand.rand(board.getHeight), width: 10,height:10,color: 'red',z:20)
-    ary.push(snail)
+    ary.push(Snail.new(x: Random.new().rand(board.getWidth), y: Random.new().rand(board.getHeight), width: 10, height: 10, color: 'black', z: 20))
     Snail.info()
     $i += 1
 end
@@ -78,18 +67,16 @@ end
 
 update do
   if tick % 10 == 0
-      $j = 0
-      while $j <= ary.length do
-          puts $j
-      snail.remove
-      snail.setXCord(1)
-      snail.setYCord(1)
-      snail.add
-        $j += 1
+      for j in 0..ary.length-1 
+          ary[j].remove
+          ary[j].x = ary[j].setXCord(ary[j].x, 10)
+          ary[j].y = ary[j].setYCord(ary[j].y, 10)
+          ary[j].add
+          j += 1
       end
 
   end
   tick += 1
 end
-puts ary
+puts ary[0]
 show
