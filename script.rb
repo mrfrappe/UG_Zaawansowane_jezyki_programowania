b3119cf28c1f764824594fc15fac08a9bd4cc9e9
Karol Kawski <karol.kawski@protonmail.com>
2018-12-31 15:47:30 +0100

    rename add colision?

--- script.rb
+++ script.rb
@@ -1,5 +1,5 @@
 require 'ruby2d'
-puts "Plantacja ślimaków"
+puts "Anthill"
 
 class Board 
     def initialize(w, h, n)
@@ -19,13 +19,12 @@ class Board
         @@name
     end
     def self.info()
-        # draw board
         puts "Boardgame (w: #@@width, h: #@@height )"
     end
 end
 
 
-class Snail < Sprite
+class Ant < Sprite
     def self.info()
     end
     def setXCord(x, w, value)
@@ -42,24 +41,24 @@ class Snail < Sprite
             y = y+10
         end
     end
-    def eat(snail)
-        if ((@x + 10>= snail.x) && (@x <= snail.x + 10 )) && ((@y + 10>= snail.y) && (@y <= snail.y + 10))
-            puts "wWykryto kolizje #{snail} x: #{snail.x}, y: #{snail.y}"
+    def colision(j, antA, ary)
+        for i in 0..ary.length-1
+            if (antA.x == ary[i].x && antA.y == ary[i].y )
+#                && j != i
+                return true
+            else
+                return false
+            end
         end
-    end
-    def copulation(snail)
-        if ((@x + 10>= snail.x) && (@x <= snail.x + 10 )) && ((@y + 10>= snail.y) && (@y <= snail.y + 10))
-            
-        end
-    end
+    end 
 end
 
 ary = Array.new 
 $i = 1
-$howManySnails = 100
+$howManyAnts = 100
 tick = 0
 
-board = Board.new(800,800, "Boardgame")
+board = Board.new(600,600, "Boardgame")
 Board.info()
 set title: board.getName
 set background: '#00B200'
@@ -68,21 +67,28 @@ set diagonostics: true
 set width: board.getWidth
 set height: board.getHeight
 
-while $i <= $howManySnails do
-    ary.push(Snail.new('art.png',x: Random.new().rand(board.getWidth), y: Random.new().rand(board.getHeight), width: 10, height: 10, z: 20))
-    Snail.info()
+while $i <= $howManyAnts do
+    ary.push(Ant.new('art.png',x: Random.new().rand(board.getWidth), y: Random.new().rand(board.getHeight), width: 10, height: 10, z: 20))
+    Ant.info()
     $i += 1
 end
 
 update do
   if tick % 10 == 0
       for j in 0..ary.length-1 
-          ary[j].eat(ary[j])
+          
           ary[j].remove
           ary[j].x = ary[j].setXCord(ary[j].x, board.getWidth, 10)
           ary[j].y = ary[j].setYCord(ary[j].y, board.getHeight, 10)
           ary[j].add
+          
+          if (ary[j].colision(j, ary[j], ary) == true)
+            puts ary[j]
+              ary[j].remove
+          end
+          
           j += 1
+          
       end
   end
   tick += 1
