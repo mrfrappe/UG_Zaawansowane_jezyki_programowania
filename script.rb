4eb3266f749390be65fe54491cc4cc9362eb0811
karolina <karolina.jedrzejewska20@gmail.com>
2019-01-15 07:11:02 +0100

    after first fix

--- script.rb
+++ script.rb
@@ -30,6 +30,10 @@ class Counter < Text
     def setText(text)
         @@text = text
     end
+    def upDateC(newText, width, height)
+	$counter.remove
+	$counter = Counter.new(text: newText, x: width - 40, y: height - 35, font: 'vera.ttf', size: 30, color: 'red', rotate: 0, z: 100)
+    end
 end
 
 class Ant < Sprite
@@ -76,7 +80,7 @@ $antWidth = 10
 $antHeight = 10
 ary = Array.new 
 toDelete = Array.new
-time = Array.new
+$time = Array.new
 tick = 0
 song = Music.new('sound.mp3')
 
@@ -90,11 +94,18 @@ set width: board.getWidth
 set height: board.getHeight
 
 
-counter = Counter.new( text: $howManyAnts, x: board.getWidth - 40, y: board.getHeight - 35, font: 'vera.ttf', size: 30, color: 'red', rotate: 0, z: 100)
+$counter = Counter.new( text: $howManyAnts, x: board.getWidth - 40, y: board.getHeight - 35, font: 'vera.ttf', size: 30, color: 'red', rotate: 0, z: 100)
 
 while $i <= $howManyAnts do
     ary.push(Ant.new('art.png',x: Random.new().rand(board.getWidth), y: Random.new().rand(board.getHeight), w: $antWidth, h: $antHeight, z: 20))
     $i += 1
+end
+
+def drawChart()
+## data must be a pre-sorted array of x,y pairs
+    chart = AsciiCharts::Cartesian.new(($time), :title => 'Time/Population').draw
+    puts "\e[H\e[2J"
+    puts chart
 end
 
 update do
@@ -109,8 +120,6 @@ update do
           ary[j].y = ary[j].setYCord(ary[j].y, board.getHeight, 10)
           ary[j].add
           
-	  #puts "j: #{j} :: x: #{ary[j].getXCord} i y: #{ary[j].getYCord}"
-
 	  ary[j].crash(j,ary[j].getXCord, ary[j].getYCord,ary,toDelete)
 
           j += 1
@@ -118,27 +127,22 @@ update do
       end
 	
       if toDelete.length != 0
-	  #puts 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
           for i in 0..toDelete.length-1
 	   #puts "jest #{toDelete.length} mrowek do usuniecia"
-	   index = toDelete.shift
-        endTime = Time.now
-        song.play
-	   ary[index].remove
-        time.push([ary.length, (endTime-startTime)])
-        counter.remove
-	   ary.delete_at(index)
-        counter= Counter.new(text: ary.length, x: board.getWidth - 40, y: board.getHeight - 35, font: 'vera.ttf', size: 30, color: 'red', rotate: 0, z: 100)
-	   #puts "mrowka #{index} usnieta"
+	    index = toDelete.shift
+            endTime = Time.now
+            song.play
+	    ary[index].remove
+            $time.push([ary.length, (endTime-startTime)])
+            
+	    ary.delete_at(index)
+            $counter.upDateC(ary.length,board.getWidth,board.getHeight)
           end
       end
   end
-    ## data must be a pre-sorted array of x,y pairs
-    chart = AsciiCharts::Cartesian.new((time), :title => 'Time/Population').draw
-    puts "\e[H\e[2J"
-    puts chart
+  drawChart
   tick += 1
-    # Close the window after 1 ant left
+  # Close the window after 1 ant left
   if ary.length == 1 then close end
 end
 show
