d1f75daa7883f7d6eac9983e17254f757caeada8
karolina <karolina.jedrzejewska20@gmail.com>
2019-01-28 05:49:45 +0100

    tird

--- script.rb
+++ script.rb
@@ -1,8 +1,6 @@
 require 'ruby2d'
 require 'ascii_charts'
 
-puts "Anthill"
-
 
 class Board 
     def initialize(w, h, n)
@@ -57,23 +55,34 @@ class Ant < Sprite
     def getYCord
 	@@y
     end
+def changeXCord(ary,j,board)
+ary[j].x = ary[j].setXCord(ary[j].x, board.getWidth, 10)
+end
+def changeYCord(ary,j,board)
+ary[j].y = ary[j].setYCord(ary[j].y, board.getHeight, 10)
+end
+
     def updatePosition(ary,j, board)
+
 	ary[j].remove
-          ary[j].x = ary[j].setXCord(ary[j].x, board.getWidth, 10)
-          ary[j].y = ary[j].setYCord(ary[j].y, board.getHeight, 10)
+changeXCord(ary,j,board)
+changeYCord(ary,j,board)
+          
+          
           ary[j].add
           
-	  ary[j].crash(j,ary[j].getXCord, ary[j].getYCord,ary,$toDelete)
+	  crash(j,ary,$toDelete)
 
     end
     
-    def crash(j, x, y, ary, toDelete)
-	stringg = "ecrash: j= #{j} j.x = #{x} j.y #{y}"
-	#puts stringg
+    def crash(j, ary, toDelete)
+	x = ary[j].getXCord
+	y = ary[j].getYCord
+	antWidth = $antWidth/2
+	antHeight = $antHeight/2
 	for i in 0..ary.length-1
 	# i!= j because the position of "j" ant is passed and we looking for another ants and which affects the ants j.
-		if (i!= j && (x-$antWidth/2...x+$antWidth/2).include?(ary[i].x) && (y-$antHeight/2...y+$antHeight/2).include?(ary[i].y))
-			#puts "do usuniecia mrowka o pozycji #{i}"
+		if (i!= j && (x-antWidth...x+antWidth).include?(ary[i].x) && (y-antHeight...y+antHeight).include?(ary[i].y))
 			#if we find another ant, we push them into toDelete array
 			$toDelete.push(i)
 		end
@@ -82,7 +91,7 @@ class Ant < Sprite
 
 end
 
-startTime = Time.new
+$startTime = Time.new
 $i = 1
 $howManyAnts = 100
 $antWidth = 10
@@ -95,20 +104,12 @@ tick = 0
 
 board = Board.new(600,600, "Boardgame")
 Board.info()
-set title: board.getName
-set background: '#00B200'
-set resizable: false
-set diagonostics: true
-set width: board.getWidth
-set height: board.getHeight
+set title: board.getName,background: '#00B200', resizable: false, diagonostics: true, width: board.getWidth, height: board.getHeight
 
 
 $counter = Counter.new( text: $howManyAnts, x: board.getWidth - 40, y: board.getHeight - 35, font: 'vera.ttf', size: 30, color: 'red', rotate: 0, z: 100)
 
-while $i <= $howManyAnts do
-    ary.push(Ant.new('art.png',x: Random.new().rand(board.getWidth), y: Random.new().rand(board.getHeight), w: $antWidth, h: $antHeight, z: 20))
-    $i += 1
-end
+
 
 def drawChart()
 ## data must be a pre-sorted array of x,y pairs
@@ -119,31 +120,41 @@ end
 def playSong()
     Music.new('sound.mp3').play
 end
+def removeAnts(index,ary)
+	ary[index].remove
+	ary.delete_at(index)
+end
+def updateTimeArry(ary)
+ 	endTime = Time.now
+	$time.push([ary.length, (endTime-$startTime)])
+end
+def updateBoardView(ary,board)
+	for i in 0..$toDelete.length-1
+	    index = $toDelete.shift
+	updateTimeArry(ary)
+            
+            playSong
+	removeAnts(index,ary)
 
+            $counter.upDateC(ary.length,board.getWidth,board.getHeight)
+        end
+end
+def insertAnts(ary,board)
+	while $i <= $howManyAnts do
+    ary.push(Ant.new('art.png',x: Random.new().rand(board.getWidth), y: Random.new().rand(board.getHeight), w: $antWidth, h: $antHeight, z: 20))
+    $i += 1
+end
+end
+
+insertAnts(ary,board)
 update do
   if tick % 10 == 0
-      # this puth should display in board
-      #puts " ARRY LENGTH: #{ary.length}"
-      #sleep(2)
       for j in 0..ary.length-1 
 	  ary[j].updatePosition(ary,j,board)        
-          
-          j += 1
-          
       end
-	
+      
       if $toDelete.length != 0
-          for i in 0..$toDelete.length-1
-	   #puts "jest #{toDelete.length} mrowek do usuniecia"
-	    index = $toDelete.shift
-            endTime = Time.now
-            playSong
-	    ary[index].remove
-            $time.push([ary.length, (endTime-startTime)])
-            
-	    ary.delete_at(index)
-            $counter.upDateC(ary.length,board.getWidth,board.getHeight)
-          end
+	updateBoardView(ary,board) 
       end
   end
   drawChart
